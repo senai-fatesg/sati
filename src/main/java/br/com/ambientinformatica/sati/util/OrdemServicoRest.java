61a8fef18307b67d23cfa439470f08f300344140
marcelinofiuza <marcelinofiuza@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-11-24 20:49:35 +0000

    Configuração do pom.xml, incluindo as dependencia para aplicação REST Configuração do web.xml, incluindo as configurações para aplicação REST Melhorias na ordem de serviço Criação do metodo OrdemServicoRest para disponibilicar WS para da ordem de serviço att Marcelino Fiuza

---
+++ src/main/java/br/com/ambientinformatica/sati/util/OrdemServicoRest.java
@@ -0,0 +1,65 @@
+package br.com.ambientinformatica.sati.util;
+
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import br.com.ambientinformatica.sati.entidade.EnumEstadoOrdemServico;
+import br.com.ambientinformatica.sati.entidade.OrdemServico;
+import br.com.ambientinformatica.sati.persistencia.OrdemServicoDao;
+
+@Path("/ordemservico")
+@Controller
+public class OrdemServicoRest {
+
+	@Autowired
+	private OrdemServicoDao ordemServicoDao;		
+	
+	private List<OrdemServico> ordensServico;
+	
+			
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<OrdemServico> listaOrdemServico(){
+		
+		return ordemServicoDao.listar();
+		//return getOrdens();
+	}
+		
+	private List<OrdemServico> getOrdens(){
+		
+		List<OrdemServico> listOS = new ArrayList<OrdemServico>();
+		OrdemServico Os = new OrdemServico();
+		Os.setDataEmissao(new Date(System.currentTimeMillis()));
+		Os.setDataFechamento(new Date(System.currentTimeMillis()));
+		Os.setDescricaoProblema("teste rest");
+		Os.setEstado(EnumEstadoOrdemServico.ATENDENDO);
+		
+		listOS.add(Os);		
+				
+		return listOS;
+	}
+	
+	
+	
+//	@GET
+//	@Path("/{idTecnico}/idTecnico")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public List<OrdemServico> findByTecnico(@PathParam("idTecnico") int idTecnico) {
+//				
+//		OrdemServicoControl osControle = new OrdemServicoControl();
+//		ordensServico = osControle.listaremAtendimento(null);
+//		return ordensServico;
+//		
+//	}
+//		
+}
