ac9a4f9b47d320a00d34fdcacf7c38a4ceafa7b6
eduardo.santana <eduardo.santana@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-12-05 18:51:32 +0000

    Pacote de Teste e Classe para teste do arquivo Relatorio.java

---
+++ src/test/java/br/com/ambientinformatica/sati/teste/RelatorioTeste.java
@@ -0,0 +1,47 @@
+package br.com.ambientinformatica.sati.teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.ambientinformatica.sati.entidade.Cliente;
+import br.com.ambientinformatica.sati.util.Relatorio;
+
+public class RelatorioTeste {
+
+	public static void main(String[] args) {
+		try {
+			List<Cliente> clientes = new ArrayList<Cliente>();
+
+			Cliente Cliente1 = new Cliente();
+			Cliente1.setNomecliente("RD Tecnologia");
+			Cliente1.setRazaoSocial("Razão Social 01");
+			Cliente1.setTelefone("3283-1110");
+			Cliente1.setCelular("98888-5566");
+			Cliente1.setEmail("teste1@email.com");
+
+			Cliente Cliente2 = new Cliente();
+			Cliente2.setNomecliente("RD Tecnologia");
+			Cliente2.setRazaoSocial("Razão Social 02");
+			Cliente2.setTelefone("3283-1110");
+			Cliente2.setCelular("98888-5566");
+			Cliente2.setEmail("teste2@email.com");
+
+			Cliente Cliente3 = new Cliente();
+			Cliente3.setNomecliente("RD Tecnologia");
+			Cliente3.setRazaoSocial("Razão Social 03");
+			Cliente3.setTelefone("3283-1110");
+			Cliente3.setCelular("98888-5566");
+			Cliente3.setEmail("teste3@email.com");
+
+			clientes.add(Cliente1);
+			clientes.add(Cliente2);
+			clientes.add(Cliente3);
+
+			Relatorio relatorio = new Relatorio();
+			relatorio.imprimir(clientes);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
