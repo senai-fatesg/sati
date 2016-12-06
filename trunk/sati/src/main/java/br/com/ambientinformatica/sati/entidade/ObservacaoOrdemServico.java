f4ad29c66d48048d718520dbe220eafd1f3e0533
wellington.franco <wellington.franco@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-12-06 02:10:05 +0000

    adicionado implements Serializable e organização do codigo.

--- trunk/sati/src/main/java/br/com/ambientinformatica/sati/entidade/ObservacaoOrdemServico.java
+++ trunk/sati/src/main/java/br/com/ambientinformatica/sati/entidade/ObservacaoOrdemServico.java
@@ -1,19 +1,30 @@
 package br.com.ambientinformatica.sati.entidade;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class ObservacaoOrdemServico {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class ObservacaoOrdemServico implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
 	private Integer id;
 	private String observacao;
 	private Date data;
 	
+	@ManyToOne
+	private OrdemServico ordemServico;
 	
 	public ObservacaoOrdemServico(){
 		
 	}
 	
-	public ObservacaoOrdemServico(String observacao, Date data) {
+	public ObservacaoOrdemServico(final String observacao,final Date data) {
 		this.observacao = observacao;
 		this.data = data;
 	}
@@ -23,13 +34,13 @@ public class ObservacaoOrdemServico {
 	public Integer getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(final Integer id) {
 		this.id = id;
 	}
 	public String getObservacao() {
 		return observacao;
 	}
-	public void setObservacao(String observacao) {
+	public void setObservacao(final String observacao) {
 		this.observacao = observacao;
 	}
 	public Date getData() {
