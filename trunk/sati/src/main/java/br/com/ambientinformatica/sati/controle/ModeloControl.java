a3ead42e6992cb28a1804c27b4a1b7f7b811fda9
carolyne <carolyne@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-12-06 22:08:21 +0000

    atvd #534 - Implementação do método responsável por listar marcas para preenchimento de selection no cadastro de modelo.

--- trunk/sati/src/main/java/br/com/ambientinformatica/sati/controle/ModeloControl.java
+++ trunk/sati/src/main/java/br/com/ambientinformatica/sati/controle/ModeloControl.java
@@ -14,33 +14,41 @@ import org.springframework.stereotype.Controller;
 
 import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
 import br.com.ambientinformatica.jpa.exception.PersistenciaException;
+import br.com.ambientinformatica.sati.entidade.Marca;
 import br.com.ambientinformatica.sati.entidade.Modelo;
+import br.com.ambientinformatica.sati.persistencia.MarcaDao;
 import br.com.ambientinformatica.sati.persistencia.ModeloDao;
 
-@Controller("ModeloControControl")
+@Controller("ModeloControl")
 @Scope("conversation")
 public class ModeloControl {
-	
-	private List<Modelo> modeloList = new ArrayList<Modelo>();
-	
-	private Modelo modelo = new Modelo();
-	
+
 	@Autowired
 	private ModeloDao modeloDao;
+
+	@Autowired
+	private MarcaDao marcaDao;
+
+	private List<Modelo> modelos = new ArrayList<Modelo>();
+
+	private List<Marca> marcas = new ArrayList<Marca>();
+
+	private Marca marcaEquipamentoSelecionada = new Marca();
 	
+	private Modelo modelo = new Modelo();
+
 	@PostConstruct
 	public void init() {
 		listar(null);
 	}
-	
+
 	public void confirmar(ActionEvent evt) {
 		try {
 			if (evt == null) {
-				FacesContext.getCurrentInstance().addMessage(
-						"Ambient Informatica",
-						new FacesMessage(FacesMessage.SEVERITY_INFO,
-								"É preciso digitar o nome!", null));
+				FacesContext.getCurrentInstance().addMessage("Ambient Informatica",
+						new FacesMessage(FacesMessage.SEVERITY_INFO, "É preciso digitar o nome!", null));
 			} else {
+				modelo.setMarcaEquipamento(marcaEquipamentoSelecionada);
 				modeloDao.alterar(modelo);
 				listar(evt);
 				inicialize(null);
@@ -50,43 +58,44 @@ public class ModeloControl {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	public void listar(ActionEvent evt) {
 		try {
-			modeloList = modeloDao.listar();
+			modelos = modeloDao.listar();
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
+	public void listarMarcas(ActionEvent evt){
+		marcas = marcaDao.listar();
+	}
+
 	public void preparaAlterar(ActionEvent evt) {
 		try {
 			modelo = (Modelo) evt.getComponent().getAttributes().get("modelo");
 			modelo = modeloDao.consultar(modelo.getId());
-			FacesContext.getCurrentInstance().getExternalContext()
-					.redirect("modeloupdate.jsf");
+			FacesContext.getCurrentInstance().getExternalContext().redirect("modeloupdate.jsf");
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e);
 		}
 	}
-	
+
 	public void excluir(ActionEvent evt) throws PersistenciaException {
 		try {
 			modelo = (Modelo) evt.getComponent().getAttributes().get("modelo");
 			modelo = modeloDao.consultar(modelo.getId());
 			modeloDao.excluirPorId(modelo.getId());
 			listar(evt);
-			FacesContext.getCurrentInstance().addMessage(
-					"Sati System",
-					new FacesMessage(FacesMessage.SEVERITY_INFO,
-							"Operação realizada com sucesso", null));
+			FacesContext.getCurrentInstance().addMessage("Sati System",
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Operação realizada com sucesso", null));
 			inicialize(null);
 		} catch (Exception e) {
 			UtilFaces.addMensagemFaces(e);
 			inicialize(null);
 		}
 	}
-	
+
 	public void inicialize(ActionEvent actionEvent) {
 		modelo = new Modelo();
 	}
@@ -98,6 +107,21 @@ public class ModeloControl {
 	public void setModelo(Modelo modelo) {
 		this.modelo = modelo;
 	}
+
+	public List<Marca> getMarcas() {
+		return marcas;
+	}
+
+	public List<Modelo> getModelos() {
+		return modelos;
+	}
+
+	public Marca getMarcaEquipamentoSelecionada() {
+		return marcaEquipamentoSelecionada;
+	}
+
+	public void setMarcaEquipamentoSelecionada(Marca marcaEquipamentoSelecionada) {
+		this.marcaEquipamentoSelecionada = marcaEquipamentoSelecionada;
+	}
 	
 }
-//by Thayro Rodrigues.
